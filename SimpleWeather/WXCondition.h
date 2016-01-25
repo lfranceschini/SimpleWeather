0defe22f48ed7a67b6521e5571f2942fbcb59d9a
Ludovic FRANCESCHINI <lfranceschini@micropole.com>
2016-01-25 12:49:39 +0100

    Add WXCondition class

---
+++ SimpleWeather/WXCondition.h
@@ -0,0 +1,30 @@
+//
+//  WXCondition.h
+//  SimpleWeather
+//
+//  Created by Ludovic FRANCESCHINI on 25/01/2016.
+//  Copyright © 2016 Ludovic FRANCESCHINI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Mantle.h>
+
+@interface WXCondition : MTLModel<MTLJSONSerializing>
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSNumber *humidity;
+@property (nonatomic, strong) NSNumber *temperature;
+@property (nonatomic, strong) NSNumber *tempHigh;
+@property (nonatomic, strong) NSNumber *tempLow;
+@property (nonatomic, strong) NSString *locationName;
+@property (nonatomic, strong) NSDate *sunrise;
+@property (nonatomic, strong) NSDate *sunset;
+@property (nonatomic, strong) NSString *conditionDescription;
+@property (nonatomic, strong) NSString *condition;
+@property (nonatomic, strong) NSNumber *windBearing;
+@property (nonatomic, strong) NSNumber *windSpeed;
+@property (nonatomic, strong) NSString *icon;
+
+- (NSString *)imageName;
+
+@end
---
+++ SimpleWeather/WXCondition.m
@@ -0,0 +1,64 @@
+//
+//  WXCondition.m
+//  SimpleWeather
+//
+//  Created by Ludovic FRANCESCHINI on 25/01/2016.
+//  Copyright © 2016 Ludovic FRANCESCHINI. All rights reserved.
+//
+
+#import "WXCondition.h"
+
+@implementation WXCondition
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+    return @{
+             @"date": @"dt",
+             @"locationName": @"name",
+             @"humidity": @"main.humidity",
+             @"temperature": @"main.temp",
+             @"tempHigh": @"main.temp_max",
+             @"tempLow": @"main.temp_min",
+             @"sunrise": @"sys.sunrise",
+             @"sunset": @"sys.sunset",
+             @"conditionDescription": @"weather.description",
+             @"condition": @"weather.main",
+             @"icon": @"weather.icon",
+             @"windBearing": @"wind.deg",
+             @"windSpeed": @"wind.speed"
+             };
+}
+
++ (NSDictionary *)imageMap {
+    
+    static NSDictionary *_imageMap = nil;
+    if (! _imageMap) {
+        
+        _imageMap = @{
+                      @"01d" : @"weather-clear",
+                      @"02d" : @"weather-few",
+                      @"03d" : @"weather-few",
+                      @"04d" : @"weather-broken",
+                      @"09d" : @"weather-shower",
+                      @"10d" : @"weather-rain",
+                      @"11d" : @"weather-tstorm",
+                      @"13d" : @"weather-snow",
+                      @"50d" : @"weather-mist",
+                      @"01n" : @"weather-moon",
+                      @"02n" : @"weather-few-night",
+                      @"03n" : @"weather-few-night",
+                      @"04n" : @"weather-broken",
+                      @"09n" : @"weather-shower",
+                      @"10n" : @"weather-rain-night",
+                      @"11n" : @"weather-tstorm",
+                      @"13n" : @"weather-snow",
+                      @"50n" : @"weather-mist",
+                      };
+    }
+    return _imageMap;
+}
+
+- (NSString *)imageName {
+    return [WXCondition imageMap][self.icon];
+}
+
+@end
