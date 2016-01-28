945b9494a381c30ede7257330e06a24288cf6ef6
Ludovic Franceschini <ludovicfranceschini@MacBook-Pro-de-Ludovic.local>
2016-01-28 22:14:50 +0100

    Add WXClient class

---
+++ WXClient.h
@@ -0,0 +1,13 @@
+//
+//  WXClient.h
+//  SimpleWeather
+//
+//  Created by Ludovic Franceschini on 28/01/2016.
+//  Copyright © 2016 Ludovic FRANCESCHINI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXClient : NSObject
+
+@end
---
+++ WXClient.m
@@ -0,0 +1,13 @@
+//
+//  WXClient.m
+//  SimpleWeather
+//
+//  Created by Ludovic Franceschini on 28/01/2016.
+//  Copyright © 2016 Ludovic FRANCESCHINI. All rights reserved.
+//
+
+#import "WXClient.h"
+
+@implementation WXClient
+
+@end
